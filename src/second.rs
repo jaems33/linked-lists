b0b84b2e088846454e0d7b07a2d594208f01c5c7
James Teow <james.teow83@gmail.com>
2021-09-14 22:15:07 -0400

    Added peek

--- src/second.rs
+++ src/second.rs
@@ -28,6 +28,19 @@ impl<T> List<T> {
             return x.elem;
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+      self.head.as_ref().map(|x|{
+        &x.elem
+      })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+      self.head.as_mut().map(|x| {
+        &mut x.elem
+      })
+    }
+
 }
 
 impl<T> Drop for List<T> {
@@ -37,6 +50,21 @@ impl<T> Drop for List<T> {
             cur_link = node.next.take();
         }
     }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+  pub fn into_iter(self) -> IntoIter<T> {
+    IntoIter(self)
+  }
+}
+
+impl<T> Iterator for IntoIter<T> {
+  type Item = T;
+  fn next(&mut self) -> Option<Self::Item> {
+    self.0.pop()
+  }
 }
 
 #[cfg(test)]
@@ -63,6 +91,35 @@ mod test {
 
       assert_eq!(list.pop(), Some(1));
       assert_eq!(list.pop(), None);
+  }
 
+  #[test]
+  fn try_peek() {
+      let mut list = List::new();      
+
+      list.push(1);
+      assert_eq!(list.peek(), Some(&1));
+
+      assert_eq!(list.peek_mut(), Some(&mut 1));
+      assert_eq!(list.peek_mut(), Some(&mut 1));
+
+      list.peek_mut().map(|n| {
+        *n = 42;
+      });
+      assert_eq!(list.peek_mut(), Some(&mut 42));
+      assert_eq!(list.pop(), Some(42));
+  }
+
+  #[test]
+  fn into_iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
   }
 }
